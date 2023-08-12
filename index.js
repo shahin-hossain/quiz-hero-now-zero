45ed3a698b2b0a1b8d098842f97eb26a93d5eb82
shahin hossain <shahinhossain3@hotmail.com>
2023-08-12 16:26:02 +0600

    question option fixed at line 68, 49

--- index.js
+++ index.js
@@ -45,8 +45,8 @@ startQuiz.addEventListener("click", () => {
 
 // All quiz data fetched from json
 const loadQuiz = async () => {
-  const res = await fetch("./data/quiz.json");
-  const data = await res.json;
+  const res = await fetch("/data/quiz.json");
+  const data = await res.json();
   quizData = data;
   displayQuiz(data);
 };
@@ -59,12 +59,13 @@ const displayQuiz = (data) => {
   }
 
   data.forEach((quiz, i) => {
+    console.log(quiz)
     quizContainer.innerHTML += `<div class="m-3 py-3 px-4 shadow-sm rounded">
   <div class="flex items-center">
     <div class="h-8 w-8 bg-green-300 rounded-full flex justify-center items-center text-green-800 mr-3">
       ${i + 1}
     </div>
-    <p class="text-gray-800 text-sm">${quiz.quetion}</p>
+    <p class="text-gray-800 text-sm">${quiz.question}</p>
   </div>
   <div class="grid grid-cols-2 gap-4 mt-5">
     ${displayQuizOptions(quiz.options, i)}
